8dcc401107632a92eed93867f1601c06f459f24f
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2020-09-25 17:00:43 +0300

    for

--- src/main/java/MyCallable.java
+++ src/main/java/MyCallable.java
@@ -4,9 +4,14 @@ public class MyCallable implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        int counter = 1;
-        while (counter < 5){
-            System.out.printf("Сообщение номер %d от потока %s\n", counter, Thread.currentThread().getName());
+        int counter = 0;
+//        while (counter < 5){
+//            System.out.printf("Сообщение номер %d от потока %s\n", counter + 1, Thread.currentThread().getName());
+//            Thread.sleep(2000);
+//            counter++;
+//        }
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Сообщение номер %d от потока %s\n", counter + 1, Thread.currentThread().getName());
             Thread.sleep(2000);
             counter++;
         }
